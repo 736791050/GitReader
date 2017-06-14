d6471000a15d0b6613c67387c9a2c714cc7186ed
lk <lk>
2017-06-14 08:46:25 +0800

    GitReader Start

---
+++ pages/index/index.js
@@ -0,0 +1,31 @@
+//index.js
+//获取应用实例
+var util = require('../../utils/util.js')
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    timeStamp : ""
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      var time = "" + util.formatTime(new Date());
+      //更新数据
+      that.setData({
+        userInfo:userInfo,
+        timeStamp:time,
+        motto:'Welcome GitReader'
+      })
+    })
+  }
+})
