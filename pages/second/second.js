bdbbfc9d1a92f64bce172b288e113b3b2c6ed4f9
lk <lk>
2017-06-29 00:13:08 +0800

    add list

--- pages/second/second.js
+++ pages/second/second.js
@@ -13,43 +13,31 @@ Page({
     autoplay: true,
     interval: 2000,
     duration: 500,
-    src: 'http://bj.bcebos.com/v1/gaopin-preview/134206530332.jpg%40%21305?authorization=bce-auth-v1%2F8297d8317b7e4592b641a2ee98e08e95%2F2017-06-16T00%3A45%3A05Z%2F60%2F%2F856fd3ef9ac990324096f6fd88e956470a53ab00239011ade6fd01878f6760d1', array: [{
-      src:'http://bj.bcebos.com/v1/gaopin-preview/134206530162.jpg%40%21305?authorization=bce-auth-v1%2F8297d8317b7e4592b641a2ee98e08e95%2F2017-06-16T00%3A45%3A05Z%2F60%2F%2F35e48f1ef0f7ce072417498cfb71fe11eff882b1eff11cb6dc09662a5cb06b90',
-      mode: 'scaleToFill',
+    cells: [
+      { position: 0, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 1, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 2, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 3, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 4, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 5, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 6, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 7, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 7, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 7, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 7, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 7, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 7, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" },
+      { position: 7, title: "今天", des: "今天银行利率调整到1.1，对于购房者和卖房者都不是好消息" }
+    ],
+    array: [{
+      src: 'http://bj.bcebos.com/v1/gaopin-preview/134206530162.jpg%40%21305?authorization=bce-auth-v1%2F8297d8317b7e4592b641a2ee98e08e95%2F2017-06-16T00%3A45%3A05Z%2F60%2F%2F35e48f1ef0f7ce072417498cfb71fe11eff882b1eff11cb6dc09662a5cb06b90',
+      mode: 'aspectFill',
       text: 'scaleToFill：不保持纵横比缩放图片，使图片完全适应'
-    }, {
-      mode: 'aspectFit',
-      text: 'aspectFit：保持纵横比缩放图片，使图片的长边能完全显示出来'
-    }, {
+    },
+    {
+      src: 'http://bj.bcebos.com/v1/gaopin-preview/134206530332.jpg%40%21305?authorization=bce-auth-v1%2F8297d8317b7e4592b641a2ee98e08e95%2F2017-06-16T00%3A45%3A05Z%2F60%2F%2F856fd3ef9ac990324096f6fd88e956470a53ab00239011ade6fd01878f6760d1',
       mode: 'aspectFill',
       text: 'aspectFill：保持纵横比缩放图片，只保证图片的短边能完全显示出来'
-    }, {
-      mode: 'top',
-      text: 'top：不缩放图片，只显示图片的顶部区域'
-    }, {
-      mode: 'bottom',
-      text: 'bottom：不缩放图片，只显示图片的底部区域'
-    }, {
-      mode: 'center',
-      text: 'center：不缩放图片，只显示图片的中间区域'
-    }, {
-      mode: 'left',
-      text: 'left：不缩放图片，只显示图片的左边区域'
-    }, {
-      mode: 'right',
-      text: 'right：不缩放图片，只显示图片的右边边区域'
-    }, {
-      mode: 'top left',
-      text: 'top left：不缩放图片，只显示图片的左上边区域'
-    }, {
-      mode: 'top right',
-      text: 'top right：不缩放图片，只显示图片的右上边区域'
-    }, {
-      mode: 'bottom left',
-      text: 'bottom left：不缩放图片，只显示图片的左下边区域'
-    }, {
-      mode: 'bottom right',
-      text: 'bottom right：不缩放图片，只显示图片的右下边区域'
     }]
   },
   //事件处理函数
